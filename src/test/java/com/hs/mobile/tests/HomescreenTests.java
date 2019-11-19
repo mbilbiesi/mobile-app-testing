b9081892ea16c6c90bf5147b3c7700b6b18c4d30
aabuhammam-hs <a.abuhammam@hungerstation.com>
2019-11-19 15:47:56 +0200

    Adding junit's test parametrization for inserting phone number, and verification code

--- src/test/java/com/hs/mobile/tests/HomescreenTests.java
+++ src/test/java/com/hs/mobile/tests/HomescreenTests.java
@@ -12,9 +12,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @Epic("Smoke Tests")
 @Feature("Homescreen Tests")
@@ -25,24 +25,28 @@ class HomescreenTests extends BaseTest {
     @Story("Open the application with a new user")
     @Description("We need to verify that all page elements are present, " +
             "and the delivery place equals current location")
-    //@Issue("https://hungerstation.atlassian.net/browse/HSAP-168")
     @Severity(SeverityLevel.CRITICAL)
     void verifyThatAllHomeScreenElementsAreDisplayed(TestInfo testInfo) {
+
         homescreenSteps.verifyThatAllHomeElementsDisplayed();
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"503263813,395406"})
     @Story("Open the application with an already registered user without any saved place")
     @Description("Login with an existing customer who doesnt have a saved address, and " +
             "verify that all home screen elements are present")
-    //@Issue("https://hungerstation.atlassian.net/browse/HSAP-169")
     @Severity(SeverityLevel.CRITICAL)
-    void verifyThatHomescreenElementsAreDisplayedForRegisteredUsers(TestInfo testInfo){
+    void verifyThatHomescreenElementsAreDisplayedForRegisteredUsers(String phoneNum, String verificationCode) {
+        String mobileNum, verification;
+        mobileNum = phoneNum;
+
+        verification = verificationCode;
         homescreenSteps.clickMyOrdersButton();
         ordersSteps.clickVerifyButton();
-        verifyAccountSteps.insertMobileNumber("503263813");
+        verifyAccountSteps.insertMobileNumber(mobileNum);
         verifyAccountSteps.clickNextButton();
-        pinCodeVerificationSteps.insertVerificationCode("395406");
+        pinCodeVerificationSteps.insertVerificationCode(verification);
         addReferalCodeSteps.clickCloseButton();
         ordersSteps.navigateToRestaurants();
         homescreenSteps.verifyThatAllHomeElementsDisplayed();
