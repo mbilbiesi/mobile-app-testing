78549585e697443b1d2a674701881859840416f3
aabuhammam-hs <a.abuhammam@hungerstation.com>
2019-11-25 14:15:54 +0200

    updating the android app apk to the latest build (6.6.0)

---
+++ src/test/java/com/hs/mobile/tests/RestaurantsListTests.java
@@ -0,0 +1,31 @@
+package com.hs.mobile.tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@Epic("Smoke Tests")
+@Feature("Restaurants List Tests")
+@Execution(ExecutionMode.CONCURRENT)
+class RestaurantsListTests extends BaseTest {
+
+    @Test
+    @Issue("HSAP-180")
+    @Story("Check Restaurants List Screen Layout")
+    @Description("Make sure that all Restaurant List objects are displayed correctly")
+    @Severity(SeverityLevel.CRITICAL)
+    public void checkRestaurantsListLayout(TestInfo testInfo){
+
+    }
+
+}
