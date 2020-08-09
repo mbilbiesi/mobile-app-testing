e9e56bd430d4b3ba9131bd73e5c85833bf754725
fahadalshehri <fahad.alshahri@hungerstation.com>
2020-08-09 12:03:07 +0300

    Added more screens

---
+++ src/test/java/com/hs/mobile/screens/ios/SelectLocationScreen.java
@@ -0,0 +1,27 @@
+package com.hs.mobile.screens.ios;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+
+public class SelectLocationScreen {
+
+    @iOSXCUITFindBy(accessibility = "back_button")
+    private MobileElement btnClose;
+
+
+    private MobileElement lblSelectLocation;
+
+    @iOSXCUITFindBy(accessibility = "search_button")
+    private MobileElement btnSearch;
+
+    private MobileElement txtAddress;
+
+    private MobileElement imgLocationMarker;
+
+    private MobileElement imgCurrentLocation;
+
+    @iOSXCUITFindBy(accessibility = "select_address_button")
+    private MobileElement btnSelectAddress;
+
+
+ }
